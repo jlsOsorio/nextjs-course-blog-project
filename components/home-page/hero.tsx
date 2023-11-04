65285735c8bee8acdc7686dcb8a0dbfaf615e07c
Joao Osorio <joao.l.osorio@gmail.com>
2023-11-04 15:17:39 +0000

    Added Hero component.

---
+++ components/home-page/hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'next/image';
+
+import styles from './hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.image}>
+        <Image
+          src="/images/site/Doug3.png"
+          alt="An image showing Doug"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Doug</h1>
+      <p>
+        I blog about web development - especially frontend framework like
+        Angular or React.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Hero from '@/components/home-page/hero';
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default HomePage;
+
+// 1) Hero (tipically called for main product) section => Present ourserlves
+// 2) Featured Posts
