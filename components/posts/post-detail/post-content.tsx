8b1d6333d2939e60080684e6e36b16d81cc0094a
Joao Osorio <joao.l.osorio@gmail.com>
2023-12-02 02:01:39 +0000

    Rendered Next Image from Markdown image content.

--- components/posts/post-detail/post-content.tsx
+++ components/posts/post-detail/post-content.tsx
@@ -4,18 +4,51 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import styles from './post-content.module.css';
 import { IPost } from '@/interfaces/post';
+import Image from 'next/image';
+import { Element } from 'react-markdown/lib';
 
 interface PostContentProps {
   post: IPost;
 }
 
+interface ParagraphProps {
+  node?: Element;
+  children?: React.ReactNode;
+}
+
 const PostContent = ({ post }: PostContentProps) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderer = {
+    p(paragraph: ParagraphProps) {
+      const { node, children } = paragraph;
+
+      if (
+        node?.children[0].type === 'element' &&
+        node?.children[0].tagName === 'img'
+      ) {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt ? (image.properties.alt as string) : ''}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{children}</p>;
+    },
+  };
 
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderer}>{post.content}</ReactMarkdown>
     </article>
   );
 };
