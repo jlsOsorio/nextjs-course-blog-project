1600d3289c18ccb54d6c43c98bf6072dfe92fe03
Joao Osorio <joao.l.osorio@gmail.com>
2023-12-16 18:46:54 +0000

    Sharp image optimization. Removed next env variables configuration.

--- next.config.js
+++ next.config.js
@@ -1,28 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase) => {
-  let env;
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    env = {
-      mongodb_username: '',
-      mongodb_password: '',
-      mongodb_clustername: '',
-      mongodb_database: '',
-    };
-  } else {
-    env = {
-      mongodb_username: '',
-      mongodb_password: '',
-      mongodb_clustername: '',
-      mongodb_database: '',
-    };
-  }
-
-  return {
-    reactStrictMode: true,
-    env,
-  };
+const nextConfig = {
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
